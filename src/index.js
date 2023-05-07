243a1a2a6696c85eda4cc7fb58cc7df10f7ae866
Marwan Rahim <marwan820@Marwan>
2023-05-07 06:19:14 -0500

    display task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  addingEventListener();
 });
+
+function addingEventListener() {
+  document
+    .getElementById("create-task-form")
+    .addEventListener("submit", handleFormSubmit);
+}
+
+function handleFormSubmit (e){
+  e.preventDefault()
+  const task = e.target[0].value
+  
+  displayTask(task)
+}
+
+function displayTask(task){
+  const taskUL = document.getElementById("tasks")
+  
+  const taskLi = document.createElement("li")
+  
+  taskLi.textContent = task
+ 
+  taskUL.appendChild(taskLi)
+}
---
+++ src/planning.md
@@ -0,0 +1,6 @@
+Stage 1 
+[x] get the form and attach an event listener
+[x] get the task from the input
+[x] display the data
+
+Stage 2 
