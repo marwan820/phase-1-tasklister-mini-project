468c21da078c025196bdc56e07f349314bd3843b
Marwan Rahim <marwan820@Marwan>
2023-05-07 22:47:47 -0500

    complete lab

--- src/sort.js
+++ src/sort.js
@@ -1,49 +1,76 @@
 document.addEventListener("DOMContentLoaded", () => {
-  addingEventListener();
-});
+   addingEventListeners()
+})
 
-function addingEventListener() {
-  document
-    .getElementById("create-task-form")
-    .addEventListener("submit", handleFormSubmit);
+let taskObjArr = []
+
+// get the form and add an event listener
+function addingEventListeners() {
+   document
+      .getElementById("create-task-form")
+      .addEventListener("submit", handleFormSubmit)
+   document.getElementById("sort-tasks").addEventListener("change", sortTasks)
 }
 
-function handleFormSubmit (e){
-  e.preventDefault()
-  const task = e.target[0].value
-  const priorityLevel = parseInt(e.target.priority.value)
-  
-  displayTask(task,priorityLevel)
+function handleFormSubmit(e) {
+   e.preventDefault()
+   console.log(e)
+   const task = e.target[0].value
+   const priorityLevel = parseInt(e.target.priority.value)
+
+   const taskObj = { task, priorityLevel }
+   taskObjArr.push(taskObj)
+
+   console.log(taskObjArr)
+
+   sortTasks()
+   displayTasks()
+
+   e.target.reset()
 }
 
-function displayTask(task,priorityLevel){
-  const taskUL = document.getElementById("tasks")
-  const taskLi = document.createElement("li")
-  const deleteBtn = document.createElement("button")
-  
-  deleteBtn.textContent = "x"
-  deleteBtn.addEventListener("click", deleteTask)
-  
-taskLi.textContent = task + " "
-taskLi.style.color = getPriorityColor(priorityLevel)
-taskLi.appendChild(deleteBtn)
- taskUL.appendChild(taskLi)
+function displayTasks() {
+   const taskUl = document.getElementById("tasks")
+   taskUl.innerHTML = ""
+
+   taskObjArr.forEach((task) => {
+      const taskLi = document.createElement("li")
+      const deleteBtn = document.createElement("button")
+
+      deleteBtn.textContent = "x"
+      deleteBtn.addEventListener("click", (e) => deleteTask(e, task))
+
+      taskLi.textContent = task.task + " "
+      taskLi.style.color = getPriorityColor(task.priorityLevel)
+      taskLi.appendChild(deleteBtn)
+      taskUl.appendChild(taskLi)
+   })
 }
 
-function deleteTask(e){
-  e.target.parentNode.remove()
-
+function deleteTask(e, task) {
+   console.log(e)
+   taskObjArr = taskObjArr.filter((element) => element.task !== task.task)
+   e.target.parentNode.remove()
 }
 
-function getPriorityColor(priorityLevel){
-if (priorityLevel === 1 ) { 
-  return "red"
-  
-} else if (priorityLevel === 2){
-  return "blue"
-  
+function getPriorityColor(priorityLevel) {
+   if (priorityLevel === 1) {
+      return "red"
+   } else if (priorityLevel === 2) {
+      return "blue"
+   } else {
+      return "green"
+   }
 }
-else { 
-  return "green"
 
-}}
+function sortTasks() {
+   console.log("in sortTasks")
+   const sortTasksSelect = document.getElementById("sort-tasks")
+   if (sortTasksSelect.value === "h-l") {
+      taskObjArr.sort((a, b) => a.priorityLevel - b.priorityLevel)
+   } else {
+      taskObjArr.sort((a, b) => b.priorityLevel - a.priorityLevel)
+   }
+   console.log(taskObjArr)
+   displayTasks()
+}
